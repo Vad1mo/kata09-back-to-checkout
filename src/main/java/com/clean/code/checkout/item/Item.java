f3d85a2502102718263e20ad56062efe7cacb62c
harsh2015 <harishpanduranga.puthran@hotmail.com>
2017-01-04 00:41:26 +0530

    REFACTOR - Removed Price Info from Item.

--- src/main/java/com/clean/code/checkout/item/Item.java
+++ src/main/java/com/clean/code/checkout/item/Item.java
@@ -1,41 +1,48 @@
 package com.clean.code.checkout.item;
 
-import java.util.Objects;
 
 public class Item {
 	
 	private String code;	
-	private Integer price;	
 	private Integer quantity = 0;
-	private Discount discount;
 	
 	
-	public Item(String code, Integer price){
+	public Item(String code){
 		this.code = code;
-		this.price = price;
 	}
-	
-	public Item(String code, Integer price, Discount discount){
-		this.code = code;
-		this.price = price;
-		this.discount = discount;
-	}
-	
-	public Integer getPrice(){
-		if(Objects.isNull(discount) || quantity < discount.getQuantity()){
-			return quantity * price;
-		}else{
-			return calculatePriceWithApplicableDiscount();
-		}		
-	}
-	
-	private Integer calculatePriceWithApplicableDiscount() {				
-		int quantityApplicableForDiscount = quantity / discount.getQuantity();
-		int quantityNotApplicableForDiscount = quantity % discount.getQuantity();		
-		return quantityApplicableForDiscount * discount.getPrice() + quantityNotApplicableForDiscount * price;
-	}
-	
+
 	public void increaseQuantity(){
 		quantity = quantity + 1;
+	}
+	
+	/**
+	 * Equals method.
+	 */
+	@Override
+	public boolean equals(Object objectToCompare){
+		if(this == objectToCompare){
+			return true;
+		}
+		if(objectToCompare == null || objectToCompare.getClass() != this.getClass()){
+			return false;
+		}
+		Item item = (Item) objectToCompare;
+		return code != null && code.equals(item.getCode());
+	}
+	
+	/**
+	 * Hashcode method.
+	 */
+	@Override
+	public int hashCode(){
+		return code.hashCode() * (22/7);
+	}
+
+	/**
+	 * Getter for code.
+	 * @return String
+	 */
+	public String getCode() {
+		return code;
 	}
 }
