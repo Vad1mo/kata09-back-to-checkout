380dd4489f114db0e807a9dc0e7e0f84d1ee30eb
harsh2015 <harishpanduranga.puthran@hotmail.com>
2017-01-03 23:32:09 +0530

    REFACTOR - Creation of Discount class.

---
+++ src/main/java/com/clean/code/checkout/item/Discount.java
@@ -0,0 +1,12 @@
+package com.clean.code.checkout.item;
+
+public class Discount {
+	
+	private Integer quantity = 0;
+	private Integer price = 0;
+	
+	public Discount(Integer quantity, Integer price) {
+		this.quantity = quantity;
+		this.price = price;		
+	}
+}
