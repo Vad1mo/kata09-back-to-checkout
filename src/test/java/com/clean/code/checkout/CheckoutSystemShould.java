5cb22330774a91b55de95c1753edb9f14dad0c03
harsh2015 <harishpanduranga.puthran@hotmail.com>
2017-01-03 22:34:22 +0530

    REFACTOR - Methods for ItemA & ItemB creation for test cases.

--- src/test/java/com/clean/code/checkout/CheckoutSystemShould.java
+++ src/test/java/com/clean/code/checkout/CheckoutSystemShould.java
@@ -19,16 +19,24 @@ public class CheckoutSystemShould {
 	
 	@Test
 	public void return_50_for_item_A(){		
-		assertEquals(50, checkoutSystem.calculateTotalPrice(asList(new Item("A", 50))));
+		assertEquals(50, checkoutSystem.calculateTotalPrice(asList(itemA())));
 	}
-	
+
 	@Test
 	public void return_30_for_item_B(){
-		assertEquals(30, checkoutSystem.calculateTotalPrice(asList(new Item("B", 30))));
+		assertEquals(30, checkoutSystem.calculateTotalPrice(asList(itemB())));
 	}
 	
 	@Test
 	public void return_80_for_items_A_and_B(){
-		assertEquals(80, checkoutSystem.calculateTotalPrice(asList(new Item("A", 50) , new Item("B", 30))));
+		assertEquals(80, checkoutSystem.calculateTotalPrice(asList(itemA() , itemB())));
+	}
+	
+	private Item itemA() {
+		return new Item("A", 50);
+	}
+	
+	private Item itemB() {
+		return new Item("B", 30);
 	}
 }
