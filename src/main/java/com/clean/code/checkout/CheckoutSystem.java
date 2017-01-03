d12f347a090e3dd2717a4e7aed3f0300c6e0553b
harsh2015 <harishpanduranga.puthran@hotmail.com>
2017-01-03 22:30:48 +0530

    REFACTOR - Updating List<Item> as input for calculateTotalPrice().

--- src/main/java/com/clean/code/checkout/CheckoutSystem.java
+++ src/main/java/com/clean/code/checkout/CheckoutSystem.java
@@ -1,22 +1,15 @@
 package com.clean.code.checkout;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import static java.util.stream.Collectors.summingInt;
+
+import java.util.List;
+
+import com.clean.code.checkout.item.Item;
 
 public class CheckoutSystem {
 	
-	private Map<String, Integer> itemPrices;
-	
-	public CheckoutSystem(){
-		itemPrices = new HashMap<>();
-		itemPrices.put("A", 50);
-		itemPrices.put("B", 30);
-	}
-
-	public int calculateTotalPrice(List<String> itemsAtCheckout) {
+	public int calculateTotalPrice(List<Item> itemsAtCheckout) {
 		return itemsAtCheckout.stream()
-				              .collect(summingInt(item -> itemPrices.get(item)));
+				              .collect(summingInt(item -> item.getPrice()));
 	}
 }
--- src/test/java/com/clean/code/checkout/CheckoutSystemShould.java
+++ src/test/java/com/clean/code/checkout/CheckoutSystemShould.java
@@ -3,25 +3,32 @@ package com.clean.code.checkout;
 import static org.junit.Assert.*;
 import static java.util.Arrays.asList;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import com.clean.code.checkout.item.Item;
 
 public class CheckoutSystemShould {
 	
+	private CheckoutSystem checkoutSystem;
+	
+	@Before
+	public void setUp(){
+		checkoutSystem = new CheckoutSystem();
+	}
+	
 	@Test
-	public void return_50_for_item_A(){
-		CheckoutSystem checkoutSystem = new CheckoutSystem();
-		assertEquals(50, checkoutSystem.calculateTotalPrice(asList("A")));
+	public void return_50_for_item_A(){		
+		assertEquals(50, checkoutSystem.calculateTotalPrice(asList(new Item("A", 50))));
 	}
 	
 	@Test
 	public void return_30_for_item_B(){
-		CheckoutSystem checkoutSystem = new CheckoutSystem();
-		assertEquals(30, checkoutSystem.calculateTotalPrice(asList("B")));
+		assertEquals(30, checkoutSystem.calculateTotalPrice(asList(new Item("B", 30))));
 	}
 	
 	@Test
 	public void return_80_for_items_A_and_B(){
-		CheckoutSystem checkoutSystem = new CheckoutSystem();
-		assertEquals(80, checkoutSystem.calculateTotalPrice(asList("A","B")));
+		assertEquals(80, checkoutSystem.calculateTotalPrice(asList(new Item("A", 50) , new Item("B", 30))));
 	}
 }
