57d3cfc4aa4a4536a6fa6d9ff030059c032a78c6
harsh2015 <harishpanduranga.puthran@hotmail.com>
2017-01-03 22:43:08 +0530

    REFACTOR - Test Class refactored.

--- src/test/java/com/clean/code/checkout/CheckoutSystemShould.java
+++ src/test/java/com/clean/code/checkout/CheckoutSystemShould.java
@@ -3,6 +3,7 @@ package com.clean.code.checkout;
 import static org.junit.Assert.*;
 import static java.util.Arrays.asList;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,32 +12,34 @@ import com.clean.code.checkout.item.Item;
 public class CheckoutSystemShould {
 	
 	private CheckoutSystem checkoutSystem;
+	private Item itemA;
+	private Item itemB;
 	
 	@Before
 	public void setUp(){
 		checkoutSystem = new CheckoutSystem();
+		itemA = new Item("A", 50);
+		itemB = new Item("B", 30);
+	}
+	
+	@After
+	public void tearDown(){
+		itemA = null;
+		itemB = null;
 	}
 	
 	@Test
 	public void return_50_for_item_A(){		
-		assertEquals(50, checkoutSystem.calculateTotalPrice(asList(itemA())));
+		assertEquals(50, checkoutSystem.calculateTotalPrice(asList(itemA)));
 	}
 
 	@Test
 	public void return_30_for_item_B(){
-		assertEquals(30, checkoutSystem.calculateTotalPrice(asList(itemB())));
+		assertEquals(30, checkoutSystem.calculateTotalPrice(asList(itemB)));
 	}
 	
 	@Test
 	public void return_80_for_items_A_and_B(){
-		assertEquals(80, checkoutSystem.calculateTotalPrice(asList(itemA() , itemB())));
-	}
-	
-	private Item itemA() {
-		return new Item("A", 50);
-	}
-	
-	private Item itemB() {
-		return new Item("B", 30);
+		assertEquals(80, checkoutSystem.calculateTotalPrice(asList(itemA, itemB)));
 	}
 }
